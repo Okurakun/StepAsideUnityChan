8180fb657f1ae26a7179c371bf506a1f26d3ef21
Okurakun <yuki-tasuku@outlook.jp>
2018-04-22 01:57:15 +0900

    StepAsideUnitychan アイテム生成
    アイテムを動的に生成・アイテム破棄を試みました。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+   
+    private GameObject myUnity;
+
+    
+	// Use this for initialization
+	void Start () {
+        this.myUnity = GameObject.Find("unitychan");
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myUnity.transform.position.z >this.transform.position.z)
+        {
+            Destroy(gameObject,3.0f);
+        }
+	}
+}
